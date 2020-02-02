57b9e4cb1a42ff59ecd49029c65749ccef07e65b
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-01 21:04:05 -0600

    Initialize Project
    Commit boilerplate code provided by Xcode. Add basic audio assets to project which will emulate the common sounds emitted from an xylophone.
    
    Related to: none

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Darren Rambaud on 02/01/2020.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    
+
+}
+
